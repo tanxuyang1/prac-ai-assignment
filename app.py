4c19288f3817b2a9e83baa8a51aca17dacb8d01f
tanxuyang1 <u2102862@siswa.um.edu.my>
2024-11-17 23:45:37 +0800

    update code

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask, request, jsonify, render_template
 from analyze import read_image
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__, template_folder='templates')
+
+SWAGGER_URL = '/api/docs'  
+API_URL = '/static/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 @app.route("/")
 def home():
